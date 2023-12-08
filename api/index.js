41e6e2c97967dd0a6e125a6b36ccceb987a4b5a6
Tsegazeab <tsegazeabwuta@gmail.com>
2023-12-08 14:04:51 -0800

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+// inside the database link we must add name of db otherwise it will automatically name it as test auth-app is our db 
+mongoose.connect(process.env.MONGODB).then(()=>console.log("DB connected")).catch(e=>console.log('DB error:', e))
 
 const app = express();
 

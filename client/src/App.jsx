c0463767389dbbe3024145eb44437aed54bebeea
Tsegazeab <tsegazeabwuta@gmail.com>
2023-12-08 12:19:45 -0800

    Add routes to every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignOut from "./pages/SignOut"
+import Profile from "./pages/Profile"
 
 function App() {
-  
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-out" element={<SignOut/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
   )
 }
 

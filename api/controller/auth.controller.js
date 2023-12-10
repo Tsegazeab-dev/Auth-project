2745283f8f64df8ccca3f9992b5a8d47a0b4658f
Tsegazeab <tsegazeabwuta@gmail.com>
2023-12-10 11:16:20 -0800

    signup page functionality added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -4,10 +4,10 @@ export const signUp = async (req, res, next)=>{
     const {username, email, password} = req.body
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({username, email, password : hashedPassword})
-    console.log(newUser)
+   
     try{
         await newUser.save();
-        console.log(newUser)
+        
         res.status(201).json({"message": "user created successfully"})
     }
     catch(error){

0e4c0be8d3e1714f864c9ebe73d1ea7666aa590f
Tsegazeab <tsegazeabwuta@gmail.com>
2023-12-10 12:37:14 -0800

    signin api route added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,17 +1,41 @@
-import User from "../model/user.model.js"
-import bcryptjs from 'bcryptjs'
-export const signUp = async (req, res, next)=>{
-    const {username, email, password} = req.body
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User({username, email, password : hashedPassword})
-   
-    try{
-        await newUser.save();
-        
-        res.status(201).json({"message": "user created successfully"})
+import { customErrorHandler } from "../../utils/error.js";
+import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+
+    res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(customErrorHandler(404, "user not found"));
     }
-    catch(error){
-        next(error)
-    }
-   
-}
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(customErrorHandler(401, "Invalid credentials"));
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
